886f1e1c48ef10ef5bc59c9efb82efca8fd60ac5
oksanatkach <otkach@tradesun.com>
2025-08-03 22:58:14 +0100

    changed paraphrase prompt approach

--- PAQ_paraphrase_search_exp_script.py
+++ PAQ_paraphrase_search_exp_script.py
@@ -7,60 +7,120 @@ from transformers import AutoTokenizer
 log_path = "/home/tmp/"
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-8B")
 
+def parse_line(line):
+    line_id, _, text = tuple(line.strip().split('\t'))
+    question, answer = tuple(text.split(' Answer: '))
+    return line_id, question, answer
+
+def get_chat_answer(question, prompt, URL):
+    data = {"question": question,
+            "prompt": prompt,
+            "temperature": 0.0, "min_tokens": 10, "n": 1, "top_n": 1.0}
+    # this should be run with FM index disabled
+    response = requests.post(f"{URL}/chat", json=data)
+    response_jsn = json.loads(response.text)
+    system_answer = response_jsn['answer']
+    return system_answer
+
+def get_tokens_hash(question, prompt, URL):
+    data = {"question": question, "prompt": prompt}
+    response = requests.post(f"{URL}/chat_get_prompt_token_ids", json=data)
+    response_jsn = json.loads(response.text)
+    prompt_token_ids = response_jsn['prompt_token_ids']
+    tokens_hash = hash(tuple(prompt_token_ids))
+    return tokens_hash
+
+def get_chat_output(question, prompt, URL):
+    data = {"question": question,
+            "prompt": prompt,
+            "temperature": 0.0, "min_tokens": 10, "n": 1, "top_n": 1.0}
+    response = requests.post(f"{URL}/chat_get_output", json=data)
+    response_jsn = json.loads(response.text)['output']
+    return response_jsn
+
+def get_beginnings(question, prompt, URL):
+    # recover input_token_ids to get hash
+    tokens_hash = get_tokens_hash(question, prompt, URL)
+    with open(f"{log_path}{tokens_hash}.beginnings", 'r') as beginnings_fh:
+        beginnings = beginnings_fh.read().strip().split('\n')
+    return beginnings
+
 def run_stage_1(FILE_I, URL):
     with open("PAQ_prompt_paraphrase_search.txt", 'r') as fh:
         prompt = fh.read().strip()
     with open(FILE_I, newline='', encoding='utf-8') as in_file:
         for line in in_file:
-            line_id, _, text = tuple(line.strip().split('\t'))
-            question, answer = tuple(text.split(' Answer: '))
-            data = {"question": question,
-                    "prompt": prompt,
-                    "temperature": 0.0, "min_tokens": 10, "n": 1, "top_n": 1.0}
-            response = requests.post(f"{URL}/chat_get_output", json=data)
-            response_jsn = json.loads(response.text)['output']
-            # TODO: match this to output structure
-            tokens_hash = hash(tuple(response_jsn['prompt_token_ids']))
-            with open(f"{log_path}{tokens_hash}.output_token_ids", 'w') as out_file:
-                json.dump(response_jsn['output_token_ids'], out_file)
+            line_id, question, answer = parse_line(line)
+            system_answer = get_chat_answer(question, prompt, URL)
+            tokens_hash = get_tokens_hash(question, prompt, URL)
+            with open(f"{log_path}{tokens_hash}.beginnings", 'w') as out_file:
+                out_file.write(system_answer)
 
-def run_stage_2(FILE_I, FILE_O, URL):
+def run_stage_2(FILE_I, URL):
+    with open("PAQ_prompt_paraphrase_search.txt", 'r') as fh:
+        paraphrase_prompt = fh.read().strip()
+
+    with open(FILE_I, newline='', encoding='utf-8') as in_file:
+        for line in in_file:
+            line_id, question, answer = parse_line(line)
+            beginnings = get_beginnings(question, paraphrase_prompt, URL)
+
+            # this should be run with FM index enabled
+            for beginning in beginnings:
+                prompt = f'Paraphrase this sentence starting with "{beginning}":'
+                response_jsn = get_chat_output(question, prompt, URL)
+                beginning_tokens_hash = hash(tuple(response_jsn['prompt_token_ids']))
+                with open(f"{log_path}{beginning_tokens_hash}.output_token_ids", 'w') as beginning_tokens_fh:
+                    json.dump(response_jsn['output_token_ids'], beginning_tokens_fh)
+
+def run_stage_3(FILE_I, FILE_O, URL):
     with open("PAQ_prompt_paraphrase_search.txt", 'r') as fh:
         paraphrase_prompt = fh.read().strip()
 
     with open("PAQ_prompt_repeat.txt", 'r') as fh:
         repeat_prompt = fh.read().strip()
 
-    with open(FILE_I, newline='', encoding='utf-8') as in_file:
+    with open(FILE_I, 'r', newline='', encoding='utf-8') as in_file:
         with open(FILE_O, 'w', newline='', encoding='utf-8') as out_file:
             for line in in_file:
-                line_id, _, text = tuple(line.strip().split('\t'))
-                question, answer = tuple(text.split(' Answer: '))
+                line_id, question, answer = parse_line(line)
+                beginnings = get_beginnings(question, paraphrase_prompt, URL)
 
-                # recover input_token_ids to get hash
-                data = {"question": question, "prompt": paraphrase_prompt}
-                response = requests.post(f"{URL}/chat_get_prompt_token_ids", json=data)
-                response_jsn = json.loads(response.text)
-                prompt_token_ids = response_jsn['prompt_token_ids']
+                beginning_tokens_lst = []
+                beginning_logprobs_lst = []
+                for beginning in beginnings:
+                    prompt = f'Paraphrase this sentence starting with "{beginning}":'
+                    beginning_tokens_hash = get_tokens_hash(question, prompt, URL)
+
+                    with open(f"{log_path}{beginning_tokens_hash}.output_token_ids", 'r') as beginning_tokens_fh:
+                        beginning_tokens = json.load(beginning_tokens_fh)
+                        beginning_tokens_lst.append(beginning_tokens)
+
+                    with open(f"{log_path}{beginning_tokens_hash}.logprobs", 'r') as beginning_logprobs_fh:
+                        beginning_logprobs = json.load(beginning_logprobs_fh)
+                        beginning_logprobs_lst.append(beginning_logprobs)
+
+                beginning_scores = [sum(beginning_logprobs) for beginning_logprobs in beginning_logprobs_lst]
+                argmax_paraphrase = max(range(len(beginning_scores)), key=lambda i: beginning_scores[i])
+                best_paraphrase = beginning_tokens[argmax_paraphrase]
 
                 # find the most successful paraphrase
-                better_question_tokens = get_best_paraphrase(prompt_token_ids)
-                better_question = tokenizer.decode(better_question_tokens)
+                # better_question_tokens = get_best_paraphrase(prompt_token_ids)
+                # better_question = tokenizer.decode(better_question_tokens)
+                better_question = tokenizer.decode(best_paraphrase)
 
+                # this should be run with FM index enabled
                 # repeat the best paraphrase and answer the question
-                data = {"question": better_question,
-                        "prompt": repeat_prompt,
-                        "temperature": 0.0, "min_tokens": 10, "n": 1, "top_n": 1.0}
-                response = requests.post(f"{URL}/chat", json=data)
-                response_jsn = json.loads(response.text)
-                system_answer = response_jsn['answer']
+                system_answer = get_chat_answer(better_question, repeat_prompt, URL)
                 out_file.write(line_id + '\t' + system_answer + '\n')
 
 def run_experiment(FILE_I, FILE_O, stage, URL):
     if stage == 1:
         run_stage_1(FILE_I, URL)
     elif stage == 2:
-        run_stage_2(FILE_I, FILE_O, URL)
+        run_stage_2(FILE_I, URL)
+    elif stage == 3:
+        run_stage_3(FILE_I, FILE_O, URL)
 
 
 if __name__ == '__main__':
