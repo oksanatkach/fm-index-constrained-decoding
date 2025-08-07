f8854b9aac49bd613786440594365d29ab174735
oksanatkach <otkach@tradesun.com>
2025-08-07 12:40:36 +0100

    adding batching to paraphrase searchg

--- PAQ_paraphrase_search_exp_script.py
+++ PAQ_paraphrase_search_exp_script.py
@@ -1,9 +1,9 @@
 import requests
 import json
 import argparse
-from typing import List, Tuple
-from paraphrase_search import get_best_paraphrase
+from typing import List
 from transformers import AutoTokenizer
+from itertools import islice
 
 log_path = "/home/tmp/"
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-8B")
@@ -54,6 +54,14 @@ def get_logprobs(tokens_hash) -> List[float]:
             key, value = line.strip().split("\t")
             logprobs.append(float(value))
     return logprobs
+
+def read_in_batches(filename, batch_size):
+    with open(filename, 'r') as file:
+        while True:
+            batch = [parse_line(line) for line in islice(file, batch_size)]
+            if not batch:
+                break
+            yield batch
 
 def run_stage_1(FILE_I, URL):
     with open("PAQ_prompt_paraphrase_search.txt", 'r') as fh:
@@ -123,6 +131,46 @@ def run_stage_3(FILE_I, FILE_O, URL):
                 system_answer = get_chat_answer(better_question, repeat_prompt, URL)
                 out_file.write(line_id + '\t' + system_answer + '\n')
 
+def run_stage_1_batch(FILE_I, URL, batch_size):
+    with open("PAQ_prompt_paraphrase_search.txt", 'r') as fh:
+        prompt = fh.read().strip()
+
+    for batch in read_in_batches(FILE_I, batch_size):
+        questions = [question for _, question, _ in batch]
+        data = {"question": questions,
+                "prompt": prompt,
+                "temperature": 0.0, "min_tokens": 10, "n": 1, "top_n": 1.0}
+        # this should be run with FM index disabled
+        response = requests.post(f"{URL}/chat_batch", json=data)
+        response_jsn = json.loads(response.text)
+        system_answers = response_jsn['answers']
+        for ind, system_answer in enumerate(system_answers):
+            system_answer = system_answer.replace('\n', '')
+            if '</think>' in system_answer:
+                system_answer = system_answer.split('</think>')[-1]
+
+            tokens_hash = get_tokens_hash(questions[ind], prompt, URL)
+            with open(f"{log_path}{tokens_hash}.beginnings", 'w') as out_file:
+                out_file.write(system_answer)
+
+def run_stage_2_batch(FILE_I, URL, batch_size):
+    with open("PAQ_prompt_paraphrase_search.txt", 'r') as fh:
+        paraphrase_prompt = fh.read().strip()
+
+    with open(FILE_I, newline='', encoding='utf-8') as in_file:
+        for line in in_file:
+            line_id, question, answer = parse_line(line)
+            beginnings = get_beginnings(question, paraphrase_prompt, URL)
+
+            # this should be run with FM index enabled
+            for beginning in beginnings:
+                prompt = f'Paraphrase this sentence starting with "{beginning}":'
+                response_jsn = get_chat_output(question, prompt, URL)
+                beginning_tokens_hash = hash(tuple(response_jsn['prompt_token_ids']))
+                with open(f"{log_path}{beginning_tokens_hash}.output_token_ids", 'w') as beginning_tokens_fh:
+                    json.dump(response_jsn['output_token_ids'], beginning_tokens_fh)
+
+
 def run_experiment(FILE_I, FILE_O, stage, URL):
     if stage == 1:
         run_stage_1(FILE_I, URL)
@@ -131,6 +179,16 @@ def run_experiment(FILE_I, FILE_O, stage, URL):
     elif stage == 3:
         run_stage_3(FILE_I, FILE_O, URL)
 
+def run_experiment_batch(FILE_I, FILE_O, stage, URL, batch_size):
+    if stage == 1:
+        run_stage_1_batch(FILE_I, URL, batch_size)
+    elif stage == 2:
+        # run_stage_2_batch(FILE_I, URL, batch_size)
+        run_stage_2(FILE_I, URL, batch_size)
+    elif stage == 3:
+        # run_stage_3_batch(FILE_I, FILE_O, URL, batch_size)
+        run_stage_3(FILE_I, FILE_O, URL, batch_size)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run PAQ experiment")
@@ -138,10 +196,18 @@ if __name__ == '__main__':
     parser.add_argument('--output', '-o', required=True, help='Path to output TSV file')
     parser.add_argument('--stage', '-s', required=True, help='Experiment stage')
     parser.add_argument('--url', '-u', default='http://127.0.0.1:8001/', help='URL for the API endpoint')
+    parser.add_argument('--batch', '-b', default=1, help='Batch size')
 
     args = parser.parse_args()
+    args.batch = int(args.batch)
 
-    run_experiment(args.input, args.output, int(args.stage), args.url)
+    assert args.batch > 0
+    if args.batch == 1:
+        run_experiment(args.input, args.output, int(args.stage), args.url)
+    else:
+        run_experiment_batch(args.input, args.output, int(args.stage), args.url, args.batch)
+
+
 
 # modify llm.py in vllm to add a function that returns input_token_ids
 # add get_output method to model api
