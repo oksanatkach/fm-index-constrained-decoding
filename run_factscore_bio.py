d1cb5e7675e6e2cb09b3cac446f2242e347c7182
oksanatkach <otkach@tradesun.com>
2025-08-08 11:10:43 +0100

    added batches to bio

--- run_factscore_bio.py
+++ run_factscore_bio.py
@@ -2,6 +2,15 @@ import csv
 import requests
 import json
 import argparse
+from itertools import islice
+
+
+def read_in_batches(reader, batch_size):
+    while True:
+        batch = [row[1] for row in islice(reader, batch_size)]
+        if not batch:
+            break
+        yield batch
 
 
 def main(I_FILE, O_FILE, URL):
@@ -11,10 +20,10 @@ def main(I_FILE, O_FILE, URL):
 
     i = 0
     for _, question in reader:
-        writer.writerow([i, question])
-        print(question)
         data = {"question": question,
-                "temperature": 0.0, "min_tokens": 10, "n": 1, "top_n": 1.0}
+                "prompt": "",
+                "min_tokens": 100, "max_tokens": 2000,
+                "temperature": 0.0, "n": 1, "top_n": 1.0}
         response = requests.post(f"{URL}/chat", json=data)
         response_jsn = json.loads(response.text)
         system_answer = response_jsn['answer']
@@ -25,6 +34,31 @@ def main(I_FILE, O_FILE, URL):
 
         i += 1
 
+def main_batch(I_FILE, O_FILE, URL, batch_size):
+    reader = csv.reader(open(I_FILE, 'r'))
+    batches = read_in_batches(reader, batch_size)
+
+    writer = csv.writer(open(O_FILE, 'w'))
+    writer.writerow(["index", "response"])
+
+    i = 0
+    for questions in batches:
+        data = {"questions": questions,
+                "prompt": "",
+                "min_tokens": 100, "max_tokens": 2000,
+                "temperature": 0.0, "n": 1, "top_n": 1.0}
+        response = requests.post(f"{URL}/chat_batch", json=data)
+        response_jsn = json.loads(response.text)
+        system_answers = response_jsn['answers']
+        for system_answer in system_answers:
+            system_answer = system_answer.replace('\n', '')
+            if '</think>' in system_answer:
+                system_answer = system_answer.split('</think>')[-1]
+            writer.writerow([i, system_answer])
+
+            i += 1
+
+
 if __name__ == "__main__":
     # "factscore-bio-data/factscore-bio-input.csv"
     # "factscore-bio-data/output_free.csv"
@@ -33,6 +67,13 @@ if __name__ == "__main__":
     parser.add_argument('--input', '-i', required=True, help='Path to input TSV file')
     parser.add_argument('--output', '-o', required=True, help='Path to output TSV file')
     parser.add_argument('--url', '-u', default='http://127.0.0.1:8001', help='URL for the API endpoint')
+    parser.add_argument('--batch', '-b', default=1, help='Batch size')
 
     args = parser.parse_args()
-    main(args.input, args.output, args.url)
+    args.batch = int(args.batch)
+
+    assert args.batch > 0
+    if args.batch == 1:
+        main(args.input, args.output, args.url)
+    else:
+        main_batch(args.input, args.output, args.url, args.batch)
